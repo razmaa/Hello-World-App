980796f2e9f132f538884110cd21296ec36e822b
razmaa <145837511+razmaa@users.noreply.github.com>
2025-05-15 11:38:17 +0400

    Creating Hello World Project
    Create Project in SwiftUI named "HelloWorldApp" using Swift.

---
+++ HelloWorldApp/HelloWorldApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorldApp
+//
+//  Created by nika razmadze on 15.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWorldApp/HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by nika razmadze on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
